06c3c835098df4176f7dad2a2e6f2ea1750fe781
bala-amypo <bala@amypo.in>
2025-12-22 17:09:12 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/UserServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+   
 
     public UserServiceImpl(UserRepository userRepository,
                            PasswordEncoder passwordEncoder) {
