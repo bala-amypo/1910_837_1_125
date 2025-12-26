f59089445e7c6ed47771044a8b2ec7df7a861782
bala-amypo <bala@amypo.in>
2025-12-26 04:46:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/MenuItem.java
+++ src/main/java/com/example/demo/entity/MenuItem.java
@@ -25,8 +25,6 @@ public class MenuItem {
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
     public BigDecimal getSellingPrice() { return sellingPrice; }
     public void setSellingPrice(BigDecimal sellingPrice) { this.sellingPrice = sellingPrice; }
     public Boolean getActive() { return active; }
